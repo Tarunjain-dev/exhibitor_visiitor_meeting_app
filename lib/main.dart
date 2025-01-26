31b6285a5d3c7e2bc530d64cdca1a8c711861d69
bhavik <kotharibhavik23@gmail.com>
2025-01-26 18:37:08 +0530

    making changes for chatting app and profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:exhibitor_visiitor_meeting_app/app/routes/app_routes.dart';
+import 'package:exhibitor_visiitor_meeting_app/app/service/firebaseService.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'app/service/storage.dart';
+import 'app/service/user.dart';
 import 'firebase_options.dart';
 
 void main() async{
@@ -10,6 +13,9 @@ void main() async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Get.put<FirebaseService>(FirebaseService());
+  await Get.putAsync<StorageService>(() => StorageService().init());
+  Get.put<UserStore>(UserStore());
   runApp(const MyApp());
 }
 
